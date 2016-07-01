ec2ca3e1e4ac9024e2d9d3a2b020cf0dbe5ff607
Mat Ryer <thatmatryer@gmail.com>
2016-07-01 16:09:39 +0100

    started adding test code

---
+++ test/test.go
@@ -0,0 +1,21 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/cheekybits/is"
+	"github.com/graymeta/stow"
+)
+
+// Test runs a generic suite of tests for Stow storage
+// implementations.
+// Passing the kind name is enough, because implementations
+// should have registered themselves via stow.Register.
+func Test(t *testing.T, kind string, config stow.Config) {
+	is := is.New(t)
+
+	location, err := stow.New(kind, config)
+	is.NoErr(err)
+	is.OK(location)
+
+}
