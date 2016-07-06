60fb56003984c5fd78ff7e52ca33efb65f1594c9
Mat Ryer <thatmatryer@gmail.com>
2016-07-06 12:32:37 +0100

    tidy up

--- walk.go
+++ walk.go
@@ -14,10 +14,12 @@ type WalkFunc func(item Item, page int, err error) error
 // Returns the first error returned by the WalkFunc or
 // nil if no errors were returned.
 func Walk(container Container, fn WalkFunc) error {
-	var err error
-	var items []Item
-	more := true
-	page := 0
+	var (
+		err   error
+		items []Item
+		more  = true
+		page  = 0
+	)
 	for more {
 		items, more, err = container.Items(page)
 		if err != nil {
