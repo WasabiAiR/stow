afa40da0780f9871d97fd84c4c9df3ff80578ae6
Mat Ryer <thatmatryer@gmail.com>
2016-07-11 17:33:45 +0100

    added test to include removing

--- test/test.go
+++ test/test.go
@@ -32,11 +32,27 @@ func All(t *testing.T, kind string, config stow.Config) {
 	c2 := createContainer(is, location, "testcontainer2")
 	is.NotEqual(c1.ID(), c2.ID())
 
+	defer func() {
+		err := location.RemoveContainer(c1.ID())
+		is.NoErr(err)
+		err = location.RemoveContainer(c2.ID())
+		is.NoErr(err)
+	}()
+
 	// add three items to c1
 	item1 := createItem(is, c1, "a_first_item", "item one")
 	item2 := createItem(is, c1, "a_second_item", "item two")
 	item3 := createItem(is, c1, "the_third_item", "item three")
 	is.OK(item1, item2, item3)
+
+	defer func() {
+		err := c1.RemoveItem(item1.ID())
+		is.NoErr(err)
+		err = c1.RemoveItem(item2.ID())
+		is.NoErr(err)
+		err = c1.RemoveItem(item2.ID())
+		is.NoErr(err)
+	}()
 
 	// get the items with a prefix (should only get 2)
 	items, _, err := c1.Items("a_", stow.CursorStart)
