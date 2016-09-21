d23b4851ca79df8a3d5e6a95a918694e3dd9a208
Piotr Rojek <piotrrojek@gmail.com>
2016-09-21 16:30:45 +0200

    Test symlinks + fix Stat -> Lstat

--- local/item_test.go
+++ local/item_test.go
@@ -2,6 +2,7 @@ package local_test
 
 import (
 	"io/ioutil"
+	"strings"
 	"testing"
 
 	"github.com/cheekybits/is"
@@ -72,7 +73,49 @@ func TestHardlink(t *testing.T) {
 			is.True(meta["is_hardlink"])
 			is.False(meta["is_symlink"])
 
-			// fmt.Println(meta["link"]) // we have to fix it, because right now it doesn't return the link
+			is.OK(meta["link"]) // we have to fix it, because right now it doesn't return the link
+		}
+	}
+}
+
+func TestSymLink(t *testing.T) {
+	is := is.New(t)
+	testDir, teardown, err := setup()
+	is.NoErr(err)
+	defer teardown()
+
+	cfg := stow.ConfigMap{"path": testDir}
+	l, err := stow.Dial(local.Kind, cfg)
+	is.NoErr(err)
+	is.OK(l)
+
+	containers, cursor, err := l.Containers("z", stow.CursorStart, 10)
+	is.NoErr(err)
+	is.OK(containers)
+	is.Equal(cursor, "")
+
+	links, err := l.Container(containers[0].ID())
+	is.NoErr(err)
+
+	items, cursor, err := links.Items("", stow.CursorStart, 10)
+	is.NoErr(err)
+	is.Equal(cursor, "")
+
+	for _, item := range items {
+		if item.Name() == "symlink" {
+			meta, err := item.Metadata()
+			is.NoErr(err)
+			is.OK(meta)
+
+			is.Equal(meta["is_dir"], false)
+			is.False(meta["is_hardlink"])
+			is.True(meta["is_symlink"])
+
+			linkStr, ok := meta["link"].(string)
+			is.True(ok)
+			is.OK(linkStr)
+
+			is.True(strings.Contains(linkStr, "symtarget"))
 		}
 	}
 }
