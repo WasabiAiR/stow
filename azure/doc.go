969dda8eef05b16265b82633564726c6e1e65aac
Piotr Rojek <piotrrojek@gmail.com>
2016-11-16 14:27:33 +0100

    Azure docs

---
+++ azure/doc.go
@@ -0,0 +1,7 @@
+// Azure stow package provides abstraction on top of Microsoft Azure Storage.
+// In order to use it, you need to pass a config with following schema:
+//     stow.ConfigMap{
+//          "account": "azure_account_id",
+//          "key": "azure_account_key",
+//          }
+package azure
