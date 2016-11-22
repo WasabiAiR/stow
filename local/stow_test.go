eb82240e3199441c2c754dee883c0e1349d19672
Piotr Rojek <piotrrojek@gmail.com>
2016-11-22 15:29:26 +0100

    Test for issue 109

--- local/stow_test.go
+++ local/stow_test.go
@@ -3,6 +3,8 @@ package local_test
 import (
 	"io/ioutil"
 	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/cheekybits/is"
@@ -19,4 +21,57 @@ func TestStow(t *testing.T) {
 	cfg := stow.ConfigMap{"path": dir}
 
 	test.All(t, "local", cfg)
+}
+
+func TestIssue109(t *testing.T) {
+	is := is.New(t)
+
+	dir, err := ioutil.TempDir("testdata", "issue109")
+	is.NoErr(err)
+	defer os.RemoveAll(dir)
+
+	err = os.MkdirAll(filepath.Join(dir, "example"), 0700)
+	is.NoErr(err)
+
+	cfg := stow.ConfigMap{"path": dir}
+
+	l, err := stow.Dial("local", cfg)
+	is.NoErr(err)
+	is.OK(l)
+
+	cs, _, _ := l.Containers("", stow.CursorStart, 10)
+	is.OK(cs)
+
+	var tmpID string
+
+	for _, c := range cs {
+		if c.Name() == "example" {
+			tmpID = c.ID()
+		}
+	}
+
+	c, err := l.Container(tmpID)
+	is.NoErr(err)
+	is.OK(c)
+
+	contents := "This is a new files stored in the cloud"
+	r := strings.NewReader(contents)
+	size := int64(len(contents))
+
+	i, err := c.Put("index.txt", r, size, nil)
+	is.NoErr(err)
+	is.OK(i)
+
+	result, err := c.Item(i.ID())
+	is.NoErr(err)
+
+	rc, err := result.Open()
+	is.NoErr(err)
+	is.OK(rc)
+
+	b, err := ioutil.ReadAll(rc)
+	is.NoErr(err)
+	is.OK(b)
+
+	is.Equal(string(b), contents)
 }
