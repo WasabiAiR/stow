81e3de67693655f759afb90e945bb9fa2e50acd9
Jason Hancock <jason@graymeta.com>
2018-10-21 21:31:39 -0700

    backblaze b2 implementation

---
+++ b2/location.go
@@ -0,0 +1,105 @@
+package b2
+
+import (
+	"errors"
+	"net/url"
+	"strings"
+
+	"github.com/graymeta/stow"
+	"gopkg.in/kothar/go-backblaze.v0"
+)
+
+type location struct {
+	config stow.Config
+	client *backblaze.B2
+}
+
+// Close closes the interface. It's a Noop for this B2 implementation
+func (l *location) Close() error {
+	return nil // nothing to close
+}
+
+// CreateContainer creates a new container (bucket)
+func (l *location) CreateContainer(name string) (stow.Container, error) {
+	bucket, err := l.client.CreateBucket(name, backblaze.AllPrivate)
+	if err != nil {
+		return nil, err
+	}
+	return &container{
+		bucket: bucket,
+	}, nil
+}
+
+// Containers lists all containers in the location
+func (l *location) Containers(prefix string, cursor string, count int) ([]stow.Container, string, error) {
+	response, err := l.client.ListBuckets()
+	if err != nil {
+		return nil, "", err
+	}
+
+	containers := make([]stow.Container, 0, len(response))
+	for _, cont := range response {
+		// api and/or library don't seem to support prefixes, so do it ourself
+		if strings.HasPrefix(cont.Name, prefix) {
+			containers = append(containers, &container{
+				bucket: cont,
+			})
+		}
+	}
+
+	return containers, "", nil
+}
+
+// Container returns a stow.Contaner given a container id. In this case, the 'id'
+// is really the bucket name
+func (l *location) Container(id string) (stow.Container, error) {
+	bucket, err := l.client.Bucket(id)
+	if err != nil || bucket == nil {
+		return nil, stow.ErrNotFound
+	}
+
+	return &container{
+		bucket: bucket,
+	}, nil
+}
+
+// ItemByURL returns a stow.Item given a b2 stow url
+func (l *location) ItemByURL(u *url.URL) (stow.Item, error) {
+	if u.Scheme != Kind {
+		return nil, errors.New("not valid b2 URL")
+	}
+
+	// b2://f001.backblaze.com/file/<container_name>/<path_to_object>
+	pieces := strings.SplitN(u.Path, "/", 4)
+
+	c, err := l.Container(pieces[2])
+	if err != nil {
+		return nil, err
+	}
+
+	filename, err := url.QueryUnescape(pieces[3])
+	if err != nil {
+		return nil, err
+	}
+	response, err := c.(*container).bucket.ListFileNames(filename, 1)
+	if err != nil {
+		return nil, stow.ErrNotFound
+	}
+
+	if len(response.Files) != 1 {
+		return nil, errors.New("unexpected number of responses from ListFileNames")
+	}
+
+	return c.Item(response.Files[0].ID)
+}
+
+// RemoveContainer removes the specified bucket. In this case, the 'id'
+// is really the bucket name
+func (l *location) RemoveContainer(id string) error {
+	stowCont, err := l.Container(id)
+	if err != nil {
+		return err
+	}
+
+	return stowCont.(*container).bucket.Delete()
+}
---
+++ b2/stow_test.go
@@ -0,0 +1,117 @@
+package b2
+
+import (
+	"math/rand"
+	"os"
+	"strings"
+	"testing"
+	"time"
+
+	isi "github.com/cheekybits/is"
+	"github.com/graymeta/stow"
+	"github.com/graymeta/stow/test"
+)
+
+func TestStow(t *testing.T) {
+	is := isi.New(t)
+	accountID := os.Getenv("B2_ACCOUNT_ID")
+	applicationKey := os.Getenv("B2_APPLICATION_KEY")
+
+	if accountID == "" || applicationKey == "" {
+		t.Skip("Backblaze credentials missing from environment. Skipping tests")
+	}
+
+	cfg := stow.ConfigMap{
+		"account_id":      accountID,
+		"application_key": applicationKey,
+	}
+
+	location, err := stow.Dial("b2", cfg)
+	is.NoErr(err)
+	is.OK(location)
+
+	t.Run("basic stow interface tests", func(t *testing.T) {
+		test.All(t, "b2", cfg)
+	})
+
+	// This test is designed to test the container.Items() function. B2 doesn't
+	// support listing items in a bucket by prefix, so our implementation fakes this
+	// functionality by requesting additional pages of files
+	t.Run("Items with prefix", func(t *testing.T) {
+		is := isi.New(t)
+		container, err := location.CreateContainer("stowtest" + randName(10))
+		is.NoErr(err)
+		is.OK(container)
+
+		defer func() {
+			is.NoErr(location.RemoveContainer(container.ID()))
+		}()
+
+		// add some items to the container
+		content := "foo"
+		item1, err := container.Put("b/a", strings.NewReader(content), int64(len(content)), nil)
+		is.NoErr(err)
+		item2, err := container.Put("b/bb", strings.NewReader(content), int64(len(content)), nil)
+		is.NoErr(err)
+		item3, err := container.Put("b/bc", strings.NewReader(content), int64(len(content)), nil)
+		is.NoErr(err)
+		item4, err := container.Put("b/bd", strings.NewReader(content), int64(len(content)), nil)
+		is.NoErr(err)
+
+		defer func() {
+			is.NoErr(container.RemoveItem(item1.ID()))
+			is.NoErr(container.RemoveItem(item2.ID()))
+			is.NoErr(container.RemoveItem(item3.ID()))
+			is.NoErr(container.RemoveItem(item4.ID()))
+		}()
+
+		items, cursor, err := container.Items("b/b", stow.CursorStart, 2)
+		is.NoErr(err)
+		is.Equal(len(items), 2)
+		is.Equal(cursor, "b/bc ")
+
+		items, cursor, err = container.Items("", stow.CursorStart, 2)
+		is.NoErr(err)
+		is.Equal(len(items), 2)
+		is.Equal(cursor, "b/bb ")
+	})
+
+	t.Run("Item Delete", func(t *testing.T) {
+		is := isi.New(t)
+		container, err := location.CreateContainer("stowtest" + randName(10))
+		is.NoErr(err)
+		is.OK(container)
+
+		defer func() {
+			is.NoErr(location.RemoveContainer(container.ID()))
+		}()
+
+		// Put an item twice, creating two versions of the file
+		content := "foo"
+		i, err := container.Put("foo", strings.NewReader(content), int64(len(content)), nil)
+		is.NoErr(err)
+		content = "foo_v2"
+		_, err = container.Put("foo", strings.NewReader(content), int64(len(content)), nil)
+		is.NoErr(err)
+
+		is.NoErr(container.RemoveItem(i.ID()))
+
+		// verify item is gone
+		_, err = container.Item(i.ID())
+		is.Equal(err, stow.ErrNotFound)
+	})
+}
+
+func randName(length int) string {
+	b := make([]rune, length)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyz")
+
+func init() {
+	rand.Seed(int64(time.Now().Nanosecond()))
+}
