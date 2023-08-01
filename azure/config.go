0266c8edb851017aa591897c2492be11eff1a3bc
Gopal K. Vashishtha <gvashishtha@anduril.com>
2023-08-01 07:23:18 -0700

    adding sovereign cloud support
    Signed-off-by: Gopal K. Vashishtha <gvashishtha@anduril.com>

--- azure/config.go
+++ azure/config.go
@@ -2,9 +2,11 @@ package azure
 
 import (
 	"errors"
+	"fmt"
 	"net/url"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
+	"github.com/Azure/go-autorest/autorest/azure"
 
 	az "github.com/Azure/azure-sdk-for-go/storage"
 	"github.com/flyteorg/stow"
@@ -12,9 +14,12 @@ import (
 
 // ConfigAccount and ConfigKey are the supported configuration items for
 // Azure blob storage.
+// SovereignCloud only supports "us" but there are other options listed
+// https://pkg.go.dev/github.com/Azure/go-autorest/autorest/azure#Environment
 const (
 	ConfigAccount = "account"
 	ConfigKey     = "key"
+	ConfigCloud   = "cloud"
 )
 
 // Kind is the kind of Location this package provides.
@@ -45,14 +50,14 @@ func init() {
 			config: config,
 		}
 
-		acc, key, err := getAccount(l.config)
+		acc, key, env, err := getAccount(l.config)
 		if err != nil {
 			return nil, err
 		}
 
 		l.account = acc
 
-		l.client, err = newBlobStorageClient(acc, key)
+		l.client, err = newBlobStorageClient(acc, key, env)
 		if err != nil {
 			return nil, err
 		}
@@ -76,22 +81,38 @@ func init() {
 	stow.Register(Kind, makefn, kindfn, validatefn)
 }
 
-func getAccount(cfg stow.Config) (account, key string, err error) {
+func getAccount(cfg stow.Config) (account, key string, env azure.Environment, err error) {
 	acc, ok := cfg.Config(ConfigAccount)
 	if !ok {
-		return "", "", errors.New("missing account id")
+		return "", "", azure.Environment{}, errors.New("missing account id")
 	}
 
 	key, ok = cfg.Config(ConfigKey)
 	if !ok {
-		return "", "", errors.New("missing auth key")
+		return "", "", azure.Environment{}, errors.New("missing auth key")
 	}
 
-	return acc, key, nil
+	cloud, ok := cfg.Config(ConfigCloud)
+	if !ok {
+		return "", "", azure.Environment{}, errors.New("missing auth key")
+	}
+	switch cloud {
+	case "us":
+		env = azure.USGovernmentCloud
+	case "german":
+		env = azure.GermanCloud
+	case "china":
+		env = azure.ChinaCloud
+	case "public":
+		env = azure.PublicCloud
+	default:
+		return "", "", azure.Environment{}, fmt.Errorf("invalid cloud %s", cloud)
+	}
+	return acc, key, env, nil
 }
 
-func newBlobStorageClient(account, key string) (*az.BlobStorageClient, error) {
-	basicClient, err := az.NewBasicClient(account, key)
+func newBlobStorageClient(account, key string, env azure.Environment) (*az.BlobStorageClient, error) {
+	basicClient, err := az.NewBasicClientOnSovereignCloud(account, key, env)
 	if err != nil {
 		return nil, errors.New("bad credentials")
 	}
